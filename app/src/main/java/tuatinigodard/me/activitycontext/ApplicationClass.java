dff6e02fa09d61bfb52687a842809a9dd519b83a
GODARD Tuatini <tuatinigodard@gmail.com>
2016-01-17 19:15:35 +0100

    Add Application class getInstance()

--- app/src/main/java/tuatinigodard/me/activitycontext/ApplicationClass.java
+++ app/src/main/java/tuatinigodard/me/activitycontext/ApplicationClass.java
@@ -6,4 +6,16 @@ import android.app.Application;
  * Created by GODARD Tuatini on 16/01/16.
  */
 public class ApplicationClass extends Application {
+
+    private static ApplicationClass instance;
+
+    public static ApplicationClass getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+    }
 }
